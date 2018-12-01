17796eec1e4239a7e1f49994260ec7b0b0e4ceaa
lepetitmoulin <36792063+lepetitmoulin@users.noreply.github.com>
2018-12-01 15:45:42 -0500

    Create day01.py

---
+++ Solutions/day01.py
@@ -0,0 +1,33 @@
+#Part 1
+
+with open('input01.txt', 'r') as f:
+	res = 0
+	for line in f:
+		num = int(line)
+		res+=num
+print(res)
+
+#solution : 510
+#solved : saturday 1 decembre 2018
+#time : 0.209s
+
+#Part 2
+
+with open('input01.txt', 'r') as f:
+	res = 0
+	seen = {0:0}
+	while seen[res]==0:
+		for line in f:
+			num = int(line)
+			res+=num
+			if res not in seen:
+				seen[res]=0
+			else:
+				seen[res]+=1
+				break
+		f.seek(0)
+	print(res)
+
+#solution : 69074
+#solved : saturday 1 decembre 2018
+#time : 0.348s
